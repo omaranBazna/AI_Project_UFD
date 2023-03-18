0a07b016f42b794842c1543212b5514e21c5cc5f
Enea Cenolli <eneacenolli@Eneas-MacBook-Pro.local>
2023-03-17 23:11:47 -0400

    working version

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,24 +6,26 @@ import emoji from "../assets/emoji.jpeg"
 
 const Home=()=>{
 /* https://developer.mozilla.org/en-US/docs/Web/HTML/Element/details */
-    return(
-      <div>
-        <div>
-          <div className="welcome">WELCOME EVERYONE</div>
-          <div className="hello">We are glad you are here</div>
-          <div className="hello">Are you ready to play?</div>
-        </div>
-        <div className="games">
-          <div><Link to="/shapes">
-            <img src={shapes} alt="shapes" />
-          </Link></div>
-          <div>
-            <Link to="/avatar">
-              <img src={emoji} alt="emoji" />
-            </Link>
-          </div>
-        </div>
-      </div>
-    )
+    return (
+			<div>
+				<div>
+					<div className="welcome">WELCOME EVERYONE</div>
+					<div className="hello">We are glad you are here</div>
+					<div className="hello">Are you ready to play?</div>
+				</div>
+				<div className="games">
+					<div>
+						<Link to="/shapes">
+							<img className="logo-home" src={shapes} alt="shapes" />
+						</Link>
+					</div>
+					<div>
+						<Link to="/avatar">
+							<img className="logo-home" src={emoji} alt="emoji" />
+						</Link>
+					</div>
+				</div>
+			</div>
+		)
 }
 export default Home
